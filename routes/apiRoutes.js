960f5aadf7a378edd96d1888080cd63cd7edeee8
rpasqualone7 <highwater92@gmail.com>
2019-05-07 21:48:40 -0400

    api routes kinda finished

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,22 +24,10 @@ module.exports = function(app) {
 
 
   
-app.post("/homepage/submit", function(req, res){
-var submit = req.body.button;
-});
-
- 
-app.post("/allocation/submit", function(req, res){
-  var submit = req.body.button;
-  });
-};
-
-app.post("/api/home/submit", function(req, res) {
-  console.log(req.body.button);
+app.post("/api/homepage/submit", function(req, res) {
   db.Post.create({
-    title: req.body.title,
-    body: req.body.body,
-    category: req.body.category
+    callo: req.body.callo,
+    numMeals: req.body.numMeals
   })
     .then(function(dbPost) {
       res.json(dbPost);
@@ -48,13 +36,24 @@ app.post("/api/home/submit", function(req, res) {
 
 
 app.post("/api/meal/submit", function(req, res) {
-  console.log(req.body);
+ 
   db.Post.create({
-    title: req.body.title,
-    body: req.body.body,
-    category: req.body.category
+  m1_foods: req.body.m1_foods,
+  m1_cal: req.body.m1_cal,
+  m2_foods: req.body.m2_foods,
+  m2_cal: req.body.m2_cal,
+  m3_foods: req.body.m3_foods,
+  m3_cal: req.body.m3_cal,
+  m4_foods: req.body.m4_foods,
+  m4_cal: req.body.m4_cal,
+  m5_foods: req.body.m5_foods,
+  m5_cal: req.body.m5_cal,
+  m6_foods: req.body.m6_foods,
+  m6_cal: req.body.m6_cal,
   })
     .then(function(dbPost) {
       res.json(dbPost);
     });
 });
+
+};
